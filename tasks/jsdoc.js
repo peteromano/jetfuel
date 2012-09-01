de64e32e04764a40d6d063141c4bbd76c0aa4ecd
Pete Romano <pete.romano@yahoo.com>
2012-09-01 16:41:04 -0400

    Logging jsdoc exec

--- tasks/jsdoc.js
+++ tasks/jsdoc.js
@@ -24,6 +24,7 @@ module.exports = function(grunt) {
             ].join(' ');
         try {
             grunt.helper('process.pushd', p);
+            grunt.log.writeln('exec: ' + cli);
             require('child_process').exec(cli);
             grunt.helper('process.popd');
         } catch(e) {
