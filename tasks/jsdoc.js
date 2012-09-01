bfc90ea734dd3f08fa5cb5a32f6f0a71071777a2
Pete Romano <pete.romano@yahoo.com>
2012-09-01 05:29:55 -0400

    Moved jsdoc dependency to core

--- tasks/jsdoc.js
+++ tasks/jsdoc.js
@@ -1,41 +1,34 @@
 module.exports = function(grunt) {
     'use strict';
 
-	var JSDOC_APP_DIR = 'app';
+    var JSDOC_APP_DIR = 'app',
+        MODULES = 'node_modules';
 
     grunt.registerMultiTask('jsdoc', 'JSDocs for src, outputted to dest.', function() {
     	var path = require('path'),
             jsdoc = this.data,
     		src = path.resolve(this.file.src),
     		dest = path.resolve(this.file.dest),
-            t = path.resolve(jsdoc.t || 'node_modules/jsdoc-toolkit/templates/jsdoc'),
-            p = jsdoc.path || 'node_modules/jsdoc-toolkit',
+            toolkitDir = path.dirname(module.filename) + '/../' + MODULES + '/jsdoc-toolkit',
+            t = path.resolve(jsdoc.t || toolkitDir + '/templates/jsdoc'),
+            p = path.resolve(jsdoc.path || toolkitDir),
             cli = [
-                JSDOC_APP_DIR+'/' + (jsdoc.app || 'run.js'),
+                path.resolve(toolkitDir + '/' + JSDOC_APP_DIR) + '/' + (jsdoc.app || 'run.js'),
                 src,
                 '-d='+dest,
                 '-t='+t,
-                jsdoc.all ? '-a' : '',
-                jsdoc.private ? '-p' : '',
-                jsdoc.recurse ? '-r='+jsdoc.recurse : '',
-                jsdoc.exclude ? '-E='+jsdoc.exclude : ''
+                jsdoc.a ? '-a' : '',
+                jsdoc.p ? '-p' : '',
+                jsdoc.r ? '-r=' + jsdoc.r : '',
+                jsdoc.E ? '-E=' + jsdoc.E : ''
             ].join(' ');
-
         try {
-
             grunt.helper('process.pushd', p);
-            grunt.log.writeln('pushd: ' + path.resolve(p));
-
             require('child_process').exec(cli);
-
-            grunt.log.writeln('exec: ' + cli);
-            grunt.log.writeln('popd: ' + grunt.helper('process.popd'));
-
+            grunt.helper('process.popd');
         } catch(e) {
-
             grunt.log.writeln(e);
             return false;
-
         }
     });
 
