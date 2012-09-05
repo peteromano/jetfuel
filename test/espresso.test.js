1dff100b669f23e5cc50b71dcad73daca5be7ea1
Pete Romano <pete.romano@yahoo.com>
2012-09-05 17:48:36 -0400

    Bumping version to fix unit test

--- test/espresso.test.js
+++ test/espresso.test.js
@@ -9,8 +9,8 @@ vows.describe('Version').addBatch({
 			return espresso.version();
 		},
 
-		'version is 0.1.26': function(topic) {
-			assert.equal(topic, '0.1.26');
+		'version is 0.1.29': function(topic) {
+			assert.equal(topic, '0.1.29');
 		}
 	} 
 
