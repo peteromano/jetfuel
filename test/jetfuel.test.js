597befbbf387ec2a32a3a5b839deb70c50c024dc
Pete Romano <pete.romano@yahoo.com>
2012-09-21 19:33:23 -0400

    Version bumps to include JetRunner

--- test/jetfuel.test.js
+++ test/jetfuel.test.js
@@ -2,15 +2,15 @@ var vows = require('vows'),
 	assert = require('assert'),
 	jetfuel = require('../lib/jetfuel');
 
-vows.describe('Jetfuel engine').addBatch({
+vows.describe('JetFuel engine').addBatch({
 
 	'core version': {
 		topic: function() {
 			return jetfuel.version();
 		},
 
-		'is 0.2.0': function(topic) {
-			assert.equal(topic, '0.2.0');
+		'is 0.2.1': function(topic) {
+			assert.equal(topic, '0.2.1');
 		}
 	} 
 
