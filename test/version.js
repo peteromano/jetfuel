a50e979a082105ba31999d81d95866febd972e38
Pete Romano <pete.romano@yahoo.com>
2012-10-09 17:05:18 -0400

    Finished logging templates

--- test/version.js
+++ test/version.js
@@ -2,7 +2,7 @@ var jetfuel = require('../lib'),
     assert = require('assert');
 
 describe('#version()', function() {
-    it('should be at 1.0.0pre-x', function() {
-        assert.ok(/1.0.0pre-\d/.test(jetfuel.version()));
+    it('should be at 1.0.0pre{version}', function() {
+        assert.ok(/1.0.0pre\d/.test(jetfuel.version()));
     })
 });
