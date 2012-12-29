3252165677d363aceee64692efa2c63fa55e88d9
Pete Romano <pete.romano@yahoo.com>
2012-12-29 11:24:25 -0500

    Version 1

--- test/version.js
+++ test/version.js
@@ -2,7 +2,7 @@ var jetfuel = require('../lib/jetfuel'),
     assert = require('assert');
 
 describe('#version()', function() {
-    it('should be at 1.0.0pre{version}', function() {
-        assert.ok(/1.0.0pre\d/.test(jetfuel.version()));
+    it('should be at 1.x', function() {
+        assert.ok(/1.\d/.test(jetfuel.version()));
     })
 });
